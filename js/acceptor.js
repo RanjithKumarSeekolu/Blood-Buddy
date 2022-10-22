96298ec3d3b0868863b70bfaad5e9bd1e5445570
Ranjith <ranjithkumarseekolu@gmail.com>
2022-10-22 21:09:42 +0530

    first commit

---
+++ js/acceptor.js
@@ -0,0 +1,4 @@
+function Successful()
+{
+     alert("Request noted, we will process it by request date");
+}
---
+++ js/registration.js
@@ -0,0 +1,7 @@
+function myFunction(){
+        if(document.getElementById("age").value>=18){
+            window.alert("Registered Successfully. Thanks a lot for Donating Blood.");
+        }
+        else
+            window.alert("Sorry to say ,Age must be greater than or equal to 18");
+}
